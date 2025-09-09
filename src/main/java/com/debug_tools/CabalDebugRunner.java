e0a96c3a95c11fd4b355e8eb53de8b944916fd47
aiquant2 <gauravraj@aiquantgroup.com>
2025-09-09 10:12:17 +0530

    fix deprecated api of taking dir path

--- src/main/java/com/debug_tools/CabalDebugRunner.java
+++ src/main/java/com/debug_tools/CabalDebugRunner.java
@@ -1,8 +1,11 @@
 package com.debug_tools;
 
+import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.execution.runners.DefaultProgramRunner;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.runners.ProgramRunner;
 import org.jetbrains.annotations.NotNull;
 //
 //public class CabalDebugRunner extends GenericProgramRunner {
@@ -19,7 +22,7 @@ import org.jetbrains.annotations.NotNull;
 //    }
 //}
 
-public class CabalDebugRunner extends DefaultProgramRunner {
+public class CabalDebugRunner implements ProgramRunner {
     @NotNull
     @Override
     public String getRunnerId() {
@@ -30,5 +33,10 @@ public class CabalDebugRunner extends DefaultProgramRunner {
     public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
         return executorId.equals(DefaultDebugExecutor.EXECUTOR_ID) &&
                 profile instanceof CabalDebugConfiguration;
+    }
+
+    @Override
+    public void execute(@NotNull ExecutionEnvironment executionEnvironment) throws ExecutionException {
+
     }
 }
